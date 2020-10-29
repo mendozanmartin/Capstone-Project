85547a6466ddb1f77bb19219eb74bf2fd0be4f10
Martin Mendoza <mendozanmartin@gmail.com>
2020-10-29 14:14:32 -0400

    add latest esp8266 code

--- ESP8266_Module_Test/ESP8266_Module_Test.ino
+++ ESP8266_Module_Test/ESP8266_Module_Test.ino
@@ -1,108 +1,147 @@
-/*
- WiFiEsp example: WebClient
- This sketch connects to google website using an ESP8266 module to
- perform a simple web search.
- For more details see: http://yaab-arduino.blogspot.com/p/wifiesp-example-client.html
-*/
+/////*
+//// WiFiEsp example: WebClient
+//// This sketch connects to google website using an ESP8266 module to
+//// perform a simple web search.
+//// For more details see: http://yaab-arduino.blogspot.com/p/wifiesp-example-client.html
+////*/
+////
+//#include "WiFiEsp.h"
+//
+//// Emulate Serial1 on pins 10/11 if not present
+////#include "SoftwareSerial.h"
+////#include "HardwareSerial.h"
+////SoftwareSerial Serial1(50,51); // RX, TX
+//
+//char ssid[] = "FBI Secret Hideout 868";            // your network SSID (name)
+//char pass[] = "Spaghetti!";        // your network password
+//int status = WL_IDLE_STATUS;     // the Wifi radio's status
+//
+//char server[] = "arduino.cc";
+//
+//// Initialize the Ethernet client object
+//WiFiEspClient client;
+//
+//void setup()
+//{
+//  // initialize serial for debugging
+//  Serial.begin(115200);
+//  // initialize serial for ESP module
+//  Serial1.begin(115200);
+//  // initialize ESP module
+//  WiFi.init(&Serial1);
+//
+//  // check for the presence of the shield
+//  if (WiFi.status() == WL_NO_SHIELD) {
+//    Serial.println("WiFi shield not present");
+//    // don't continue
+//    while (true);
+//  }
+//
+//  // attempt to connect to WiFi network
+//  while ( status != WL_CONNECTED) {
+//    Serial.print("Attempting to connect to WPA SSID: ");
+//    Serial.println(ssid);
+//    // Connect to WPA/WPA2 network
+//    status = WiFi.begin(ssid, pass);
+//  }
+//
+//  // you're connected now, so print out the data
+//  Serial.println("You're connected to the network");
+//
+//  printWifiStatus();
+//
+//  Serial.println();
+//  Serial.println("Starting connection to server...");
+//  // if you get a connection, report back via serial
+//  if (client.connect(server, 80)) {
+//    Serial.println("Connected to server");
+//    // Make a HTTP request
+//    String content = "Hey, just testing a post request.";
+//    client.println("POST https://47989dcaec57.ngrok.io/data HTTP/1.1");
+//    client.println("Host: SERVER:PORT");
+//    client.println("Accept: */*");
+//    client.println("Content-Length: " + content.length());
+//    client.println("Content-Type: application/x-www-form-urlencoded");
+//    client.println();
+//    client.println(content);
+//  }
+//}
+//
+//void loop()
+//{
+//  // if there are incoming bytes available
+//  // from the server, read them and print them
+//  while (client.available()) {
+//    char c = client.read();
+//    Serial.write(c);
+//  }
+//
+//  // if the server's disconnected, stop the client
+//  if (!client.connected()) {
+//    Serial.println();
+//    Serial.println("Disconnecting from server...");
+//    client.stop();
+//
+//    // do nothing forevermore
+//    while (true);
+//  }
+//}
+//
+//
+//void printWifiStatus()
+//{
+//  // print the SSID of the network you're attached to
+//  Serial.print("SSID: ");
+//  Serial.println(WiFi.SSID());
+//
+//  // print your WiFi shield's IP address
+//  IPAddress ip = WiFi.localIP();
+//  Serial.print("IP Address: ");
+//  Serial.println(ip);
+//
+//  // print the received signal strength
+//  long rssi = WiFi.RSSI();
+//  Serial.print("Signal strength (RSSI):");
+//  Serial.print(rssi);
+//  Serial.println(" dBm");
+//
+//}
+#include <SoftwareSerial.h>
 
-#include "WiFiEsp.h"
-
-// Emulate Serial1 on pins 10/11 if not present
-#ifndef HAVE_HWSERIAL1
-#include "SoftwareSerial.h"
-SoftwareSerial Serial1(18, 19); // RX, TX
-#endif
-
-char ssid[] = "FBI Secret Hideout 868";            // your network SSID (name)
-char pass[] = "Spaghetti!";        // your network password
-int status = WL_IDLE_STATUS;     // the Wifi radio's status
-
-char server[] = "arduino.cc";
-
-// Initialize the Ethernet client object
-WiFiEspClient client;
+SoftwareSerial ESPserial(10,11); // RX | TX
 
 void setup()
+
 {
-  // initialize serial for debugging
-  Serial.begin(115200);
-  // initialize serial for ESP module
-  Serial1.begin(9600);
-  // initialize ESP module
-  WiFi.init(&Serial1);
 
-  // check for the presence of the shield
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
-    // don't continue
-    while (true);
-  }
+Serial.begin(115200); // communication with the host computer
 
-  // attempt to connect to WiFi network
-  while ( status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network
-    status = WiFi.begin(ssid, pass);
-  }
+//while (!Serial) { ; }
 
-  // you're connected now, so print out the data
-  Serial.println("You're connected to the network");
+// Start the software serial for communication with the ESP8266
 
-  printWifiStatus();
+ESPserial.begin(115200);
 
-  Serial.println();
-  Serial.println("Starting connection to server...");
-  // if you get a connection, report back via serial
-  if (client.connect(server, 80)) {
-    Serial.println("Connected to server");
-    // Make a HTTP request
-    String content = "Hey, just testing a post request.";
-    client.println("POST https://47989dcaec57.ngrok.io/data HTTP/1.1");
-    client.println("Host: SERVER:PORT");
-    client.println("Accept: */*");
-    client.println("Content-Length: " + content.length());
-    client.println("Content-Type: application/x-www-form-urlencoded");
-    client.println();
-    client.println(content);
-  }
+Serial.println("");
+
+Serial.println("Remember to to set Both NL & CR in the serial monitor.");
+
+Serial.println("Ready");
+
+Serial.println("");
+
 }
 
 void loop()
+
 {
-  // if there are incoming bytes available
-  // from the server, read them and print them
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-  }
 
-  // if the server's disconnected, stop the client
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("Disconnecting from server...");
-    client.stop();
+// listen for communication from the ESP8266 and then write it to the serial monitor
 
-    // do nothing forevermore
-    while (true);
-  }
-}
+if ( ESPserial.available() ) { Serial.write( ESPserial.read() ); }
 
+// listen for user input and send it to the ESP8266
 
-void printWifiStatus()
-{
-  // print the SSID of the network you're attached to
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+if ( Serial.available() ) { ESPserial.write( Serial.read() ); }
 
-  // print your WiFi shield's IP address
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print the received signal strength
-  long rssi = WiFi.RSSI();
-  Serial.print("Signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
 }
