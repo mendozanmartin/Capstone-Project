c41f53e76cefff0bb32cfc1dc900f5349dd03a48
Martin Mendoza <mendozanmartin@gmail.com>
2020-12-03 13:52:02 -0500

    cleaned up code + automation:

--- Final Code/Final_Code/Final_Code.ino
+++ Final Code/Final_Code/Final_Code.ino
@@ -13,9 +13,7 @@
 #define tdsSensorPin A1
 #define turbiditySensorPin A2
 #define flowSensorPin 8
-
-#define VREF 5.0  // analog reference voltage(Volt) of the ADC
-#define SCOUNT 30 // sum of sample point
+#define inletValvePin 9
 
 char ssid[] = WIFI_SSID;     // your network SSID (name)
 char pass[] = WIFI_PASSWORD; // your network password
@@ -24,26 +22,17 @@ char adafruitKey[] = ADAFRUIT_IO_KEY;
 char server[] = MQTT_SERVER;
 int port = MQTT_PORT;
 
-// int status = WL_IDLE_STATUS; // the Wifi radio's status
-
-bool pulseSignal, lastPulseSignal = false;
-float tdsValue = 0;
-float levelSensorValue = 0;
-
-int pulseCount = 0;
-float flowRate = 0;
-int countArray[5] = {0, 0, 0, 0, 0};
-unsigned long delta_t, final_t;
-unsigned long initial_t = 0;
-
 void callback(char *topic, byte *payload, unsigned int length);
 void reconnect();
 
-int j = 0;
+float LEVEL_THRESHOLD = 10;
+int PUBLISH_INTERVAL = 2000;
 
+//Timer declaration
 Timer mqttLoop;
 Timer sensorLoop;
-int PUBLISH_INTERVAL = 2000;
+Timer controlLoop;
+
 SimpleWifi simpleWifi;
 
 //Sensors Declaration
@@ -51,6 +40,7 @@ TdsSensor tdsSensor(tdsSensorPin);
 LevelSensor levelSensor(pressureSensorPin);
 TurbidityLib turbiditySensor(turbiditySensorPin);
 FlowMeter flowSensor(flowSensorPin);
+ValveCtrl valve(inletValvePin);
 
 void setup()
 {
@@ -69,9 +59,9 @@ void setup()
 
   pinMode(LED_BUILTIN, OUTPUT);
   mqttLoop.every(1000, mqttConnect);
-  // sensorLoop.every((PUBLISH_INTERVAL * 4 + 3), sensorPublish); // give it extra 2 seconds so that function calls do not overlap
-    sensorLoop.every((PUBLISH_INTERVAL), sensorPublish); // give it extra 2 seconds so that function calls do not overlap
-
+  controlLoop.every(1500, controlValve);
+  sensorLoop.every((PUBLISH_INTERVAL * 4 + 3), publishSensorReadings); // give it extra 2 seconds so that function calls do not overlap
+  // sensorLoop.every((PUBLISH_INTERVAL), sensorPublish); // give it extra 2 seconds so that function calls do not overlap
 }
 
 void mqttConnect()
@@ -82,7 +72,6 @@ void mqttConnect()
     simpleWifi.connectToMqtt(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY, server);
 
     boolean isSubscribed = simpleWifi.mqttSubscribe("mendozamartin/feeds/outlet-valve", 1);
-    boolean isSubscribed2 = simpleWifi.mqttSubscribe("mendozamartin/feeds/inlet-valve", 1);
 
     if (isSubscribed)
     {
@@ -97,30 +86,43 @@ void mqttConnect()
   simpleWifi.mqttLoop();
 }
 
+void controlValve()
+{
+  if (levelSensor.getReading() >= LEVEL_THRESHOLD)
+  {
+    valve.closeValve();
+    simpleWifi.mqttPublish("mendozamartin/feeds/outlet-valve", "OFF");
+  }
+  else
+  {
+    valve.openValve();
+    simpleWifi.mqttPublish("mendozamartin/feeds/outlet-valve", "ON");
+  }
+}
 
-void sensorPublish()
+void publishSensorReadings()
 {
   char msgBuffer[10]; // make sure this is big enough to hold your string
 
-  // levelSensorValue = levelSensor.getReading();
-  // simpleWifi.mqttPublish("mendozamartin/feeds/collection-tank-level", dtostrf(levelSensorValue, 6, 2, msgBuffer));
+  float levelSensorValue = levelSensor.getReading();
+  simpleWifi.mqttPublish("mendozamartin/feeds/collection-tank-level", dtostrf(levelSensorValue, 6, 2, msgBuffer));
 
-  /////////////////////////////////////////////////////////////////////////////////////////
-  // delay(PUBLISH_INTERVAL);
-  // turbiditySensor.startSampling();
-  // float NTU = turbiditySensor.getReading(); // read the input on analog pin 0:
-  // simpleWifi.mqttPublish("mendozamartin/feeds/collection-tank-turbidity", dtostrf(NTU, 6, 2, msgBuffer));
-  
-  ////////////////////////////////////////////////////////////////////////////////////////
-  // delay(PUBLISH_INTERVAL);
+  ///////////////////////////////////////////////////////////////////////////////////////
+  delay(PUBLISH_INTERVAL);
+  turbiditySensor.startSampling();
+  float NTU = turbiditySensor.getReading(); // read the input on analog pin 0:
+  simpleWifi.mqttPublish("mendozamartin/feeds/collection-tank-turbidity", dtostrf(NTU, 6, 2, msgBuffer));
 
-  // tdsSensor.startSampling();
-  // tdsValue = tdsSensor.getReading();
-  // simpleWifi.mqttPublish("mendozamartin/feeds/collection-tank-tds", dtostrf(tdsValue, 6, 2, msgBuffer));
+  //////////////////////////////////////////////////////////////////////////////////////
+  delay(PUBLISH_INTERVAL);
 
-  /////////////////////////////////////////////////////////////////////////////////////////
-  // delay(PUBLISH_INTERVAL);
-  flowRate = flowSensor.getReading();
+  tdsSensor.startSampling();
+  float tdsValue = tdsSensor.getReading();
+  simpleWifi.mqttPublish("mendozamartin/feeds/collection-tank-tds", dtostrf(tdsValue, 6, 2, msgBuffer));
+
+  ///////////////////////////////////////////////////////////////////////////////////////
+  delay(PUBLISH_INTERVAL);
+  float flowRate = flowSensor.getReading();
   simpleWifi.mqttPublish("mendozamartin/feeds/inlet-flowrate", dtostrf(flowRate, 6, 2, msgBuffer));
 }
 
@@ -128,6 +130,7 @@ void loop()
 {
   mqttLoop.update();
   sensorLoop.update();
+  controlLoop.update();
   flowSensor.flowSampling();
 }
 
@@ -146,11 +149,11 @@ void callback(char *topic, byte *payload, unsigned int length)
   if (message[1] == 'N')
   {
     Serial.println(message);
-    digitalWrite(LED_BUILTIN, HIGH);
+    valve.openValve();
   }
   else if (message[1] == 'F')
   {
     Serial.println(message);
-    digitalWrite(LED_BUILTIN, LOW);
+    valve.closeValve();
   }
 }
